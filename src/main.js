add2149dcabc1d75361499f499b7bb178ed78d39
dodinhtrung97 <dodinhtrung97@gmail.com>
2018-05-14 19:12:42 +0700

    Can create and display notes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App'
+import router from './router'
 
 /* eslint-disable no-new */
-new Vue({
-  el: 'body',
+const app = new Vue({
+  el: '#app',
+  router,
+  render: h => h(App),
   components: { App }
-})
+});
