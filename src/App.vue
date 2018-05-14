6c6ff630475042c50f3043b682514b5a5ffb13b3
hart_suvanadat@hotmail.com <hart_suvanadat@hotmail.com>
2018-05-14 19:17:11 +0700

    Added Google Keep template

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,119 @@
-<style>
-
-* {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-}
-
-html {
-    position: relative;
-    min-height: 100%;
-    font-family: sans-serif;
-}
-
-body {
-    background: #eee;
-    min-height: 100%;
-}
-
-body > div {
-    padding: 16px 16px 50px;
-    min-height: 100vh;
-    background: #eee;
-    position: relative;
-    margin-bottom: 50px;
-    z-index: 1;
-    box-shadow: 0 2px 5px #111;
-}
-
-footer {
-    background: #333;
-    color: #fff;
-    text-align: center;
-    padding: 12px 0 10px;
-    position: fixed;
-    bottom: 0;
-    height: 50px;
-    width: 100%;
-}
-
-footer a {
-    color: #e03c3c;
-}
-
-footer .fa-heart {
-    margin: 0 10px 0 0;
-}
-
-footer iframe {
-    position: relative;
-    top: 4px;
-}
-
-</style>
-
 <template>
-
-<div>
-    <create-note-form></create-note-form>
-    <notes></notes>
-</div>
-
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      clipped
+      class="grey lighten-4"
+      app
+    >
+      <v-list
+        dense
+        class="grey lighten-4"
+      >
+        <template v-for="(item, i) in items">
+          <v-layout
+            v-if="item.heading"
+            :key="i"
+            row
+            align-center
+          >
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-flex>
+            <v-flex xs6 class="text-xs-right">
+              <v-btn small flat>edit</v-btn>
+            </v-flex>
+          </v-layout>
+          <v-divider
+            v-else-if="item.divider"
+            :key="i"
+            dark
+            class="my-3"
+          ></v-divider>
+          <v-list-tile
+            v-else
+            :key="i"
+            @click=""
+          >
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="grey--text">
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="amber" app absolute clipped-left>
+      <v-toolbar-side-icon @click.native="drawer = !drawer"></v-toolbar-side-icon>
+      <span class="title ml-3 mr-5">Google&nbsp;<span class="text">Keep</span></span>
+      <v-text-field
+        solo-inverted
+        flat
+        label="Search"
+        prepend-icon="search"
+      ></v-text-field>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-container class="main">
+      <create-note-form></create-note-form>
+      <notes></notes>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
+  import Notes from './components/notes/Index'
+  import CreateNoteForm from './components/notes/Create'
 
-import Notes from './components/notes/Index'
-import CreateNoteForm from './components/notes/Create'
-export default {
+  export default {
+    data: () => ({
+      drawer: null,
+      items: [
+        { icon: 'lightbulb_outline', text: 'Notes' },
+        { icon: 'touch_app', text: 'Reminders' },
+        { divider: true },
+        { heading: 'Labels' },
+        { icon: 'add', text: 'Create new label' },
+        { divider: true },
+        { icon: 'archive', text: 'Archive' },
+        { icon: 'delete', text: 'Trash' },
+        { divider: true },
+        { icon: 'settings', text: 'Settings' },
+        { icon: 'chat_bubble', text: 'Trash' },
+        { icon: 'help', text: 'Help' },
+        { icon: 'phonelink', text: 'App downloads' },
+        { icon: 'keyboard', text: 'Keyboard shortcuts' }
+      ]
+    }),
+    props: {
+      source: String
+    },
     components: {
-        Notes,
-        CreateNoteForm
+      Notes,
+      CreateNoteForm
     }
-}
-
+  }
 </script>
+
+<style>
+  #keep main .container {
+    height: 660px;
+  }
+  .navigation-drawer__border {
+    display: none;
+  }
+  .text {
+    font-weight: 400;
+  }
+
+  .main {
+    padding-top: 80px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from "vuetify";
+
+Vue.use(Vuetify);
+
 
 /* eslint-disable no-new */
 const app = new Vue({
