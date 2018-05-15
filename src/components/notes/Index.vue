856b5eed37e77ee104d91e78f25b34219ef70d36
hart_suvanadat@hotmail.com <hart_suvanadat@hotmail.com>
2018-05-16 05:32:24 +0700

    Now uses orderKey, fix various bugs with reordering, improved usage of store

--- src/components/notes/Index.vue
+++ src/components/notes/Index.vue
@@ -11,71 +11,86 @@
 
 <template>
 
-<div class="notes" ref="notes">
-    <note v-for="note in this.$store.state.notes"
-          :note="note">
-    </note>
-</div>
+    <div class="notes" ref="notes">
+        <note v-for="note in this.$store.state.notes"
+              :note="note"
+              :key="note.key">
+        </note>
+    </div>
 
 </template>
 
 <script>
 
-import Packery from 'packery'
-import Draggabilly from 'draggabilly'
-import Note from './Note'
-import { db } from '../../firebase'
+    import Packery from 'packery'
+    import Draggabilly from 'draggabilly'
+    import Note from './Note'
+    import { db } from '../../firebase'
+    import { mapMutations } from 'vuex'
 
-export default {
-    components: {
-        Note
-    },
-    mounted() {
-        this.$store.state.notes = [];
-        let packery = new Packery(this.$refs.notes, {
-            itemSelector: '.note',
-            columnWidth: 240,
-            gutter: 16,
-            fitWidth: true
-        });
-        let currentIndex = 0;
-         db.ref('notes').on('child_added', (snapshot) => {
-            let note = {title: snapshot.val().title,
-                        content: snapshot.val().content,
-                        key: snapshot.key};
-            this.$store.state.notes.unshift(note);
-            this.$nextTick(() => { // the new note hasn't been rendered yet, but in the nextTick, it will be rendered
-              //https://codepen.io/anon/pen/NMBvLM check here for more info
-              let items = packery.getItemElements();
-              for (let i = currentIndex; i < items.length; i++) {
-                let draggie = new Draggabilly(items[i]);
-                packery.bindDraggabillyEvents(draggie);
-                currentIndex++;
-              }
-              packery.reloadItems();
-              packery.layout();
-            })
-        });
+    export default {
 
-        packery.on( 'dragItemPositioned', () => {
-          let currentIndex = 0;
-          const items = packery.getItemElements();
-          for (let i = currentIndex; i < items.length; i++) {
-            let title = items[i].getElementsByTagName("h1")[0].innerHTML;
-            let content = items[i].getElementsByTagName("pre")[0].innerHTML;
-            let key = this.$store.state.notes[i].key;
-            if (title === this.$store.state.notes[i].title && content === this.$store.state.notes[i].content) {
-              continue;
-            }
-            let note = {title: title,
+        components: {
+            Note
+        },
+        methods: {
+            ...mapMutations([
+                'addNote',
+                'clearNotes',
+                'incrementOrderKey',
+                'setUpdatedNotes',
+                'incrementLastCheckedIndex'])
+        },
+        mounted() {
+            this.clearNotes(); // just for when code is updated, so that you don't need to refresh
+            let packery = new Packery(this.$refs.notes, {
+                itemSelector: '.note',
+                columnWidth: 240,
+                gutter: 16,
+                fitWidth: true
+            });
+            db.ref('notes').orderByChild("orderKey").on('child_added', (snapshot) => {
+                let note = {title: snapshot.val().title,
+                    content: snapshot.val().content,
+                    key: snapshot.key};
+                this.addNote(note);
+                this.incrementOrderKey();
+                this.$nextTick(() => { // the new note hasn't been rendered yet, but in the nextTick, it will be rendered
+                    //https://codepen.io/anon/pen/NMBvLM check here for more info
+                    packery.reloadItems();
+                    let items = packery.getItemElements().reverse(); // Workaround to make it easier to keep track of new elements added
+                    let currentIndex = this.$store.state.lastCheckedIndex;
+                    for (currentIndex; currentIndex < items.length; currentIndex++) {
+                        let draggie = new Draggabilly(items[currentIndex]);
+                        packery.bindDraggabillyEvents(draggie);
+                        this.incrementLastCheckedIndex();
+                    }
+                    packery.layout()
+                })
+            });
+
+            packery.on( 'dragItemPositioned', () => {
+                let items = packery.getItemElements();
+                let currentIndex = items.length-1;
+                let newNotes = [];
+                for (let i = 0; i < items.length; i++) {
+                    let title = items[i].getElementsByTagName("h1")[0].innerHTML;
+                    let content = items[i].getElementsByTagName("pre")[0].innerHTML;
+                    let key = items[i].children[2].innerHTML;
+                    let note = {
+                        title: title,
                         content: content,
-                        key: key};
-            this.$store.state.notes[i] = note;
-            db.ref('notes/').child(key).set({title:title, content:content});
-            currentIndex++;
-          }
-        });
+                        key: key
+                    };
+                    newNotes.push(note);
+                    let updates = {};
+                    updates['notes/' + key + '/orderKey'] = currentIndex;
+                    db.ref().update(updates);
+                    currentIndex--;
+                }
+                this.setUpdatedNotes(newNotes);
+            });
+        }
     }
-}
 
 </script>
