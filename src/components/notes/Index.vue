ed867fe321b79d61da699fdaa6fea799bcded2d7
dodinhtrung97 <dodinhtrung97@gmail.com>
2018-05-15 08:19:56 +0700

    Notes working

--- src/components/notes/Index.vue
+++ src/components/notes/Index.vue
@@ -21,7 +21,7 @@
 
 <script>
 
-import Firebase from 'firebase'
+import { db } from '../../firebase'
 import Packery from 'packery'
 import Draggabilly from 'draggabilly'
 import Note from './Note'
@@ -38,12 +38,11 @@ export default {
             gutter: 16,
             fitWidth: true
         });
-        let firebase = new Firebase('https://keep-student-edition.firebaseio.com');
         let currentIndex = 0;
-        firebase.child('notes').on('child_added', (snapshot) => {
+        db.ref('notes').on('child_added', (snapshot) => {
             let note = {title: snapshot.val().title,
                         content: snapshot.val().content,
-                        key: snapshot.key()};
+                        key: snapshot.ref};
             this.$store.state.notes.unshift(note);
             this.$nextTick(() => { // the new note hasn't been rendered yet, but in the nextTick, it will be rendered
               //https://codepen.io/anon/pen/NMBvLM check here for more info
